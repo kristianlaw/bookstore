7f6e5b000a65c6654e04840f4ab793567d73dac5
lawkr <lawkristian@hotmail.com>
2020-03-04 12:21:49 +0200

    Update

--- bookstore/src/main/java/com/example/bookstore/BookController/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/BookController/BookController.java
@@ -1,16 +1,46 @@
 package com.example.bookstore.BookController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.model.domain.BookRepository;;
 
 @Controller
 
 public class BookController {
 	
-	@RequestMapping("/index")
-	public String greeting() {
+	@Autowired
+	
+	private BookRepository repository;
+	
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+	repository.deleteById(bookId);
+	return "redirect:../booklist";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model){
+	model.addAttribute("book", new Book());
+	
+	return "addbook";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+		public String save(Book book){
+	repository.save(book);
+	return "redirect:booklist";
+	}
+	
+	@RequestMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
 		
-		return "index";
+		return "booklist";
 	}
 }
--- bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,13 +1,34 @@
 package com.example.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore.model.domain.Book;
+import com.example.bookstore.model.domain.BookRepository;
+
+
+
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+	return (args) -> {
+		Book a = new Book("Harry Potter", "J.K Rowlings", 101010, 2009, 20);
+		Book b = new Book("Aku Ankka", "Don Rosa", 202020, 2005, 5);
+		Book c = new Book("Raamattu", "Multiple authors", 303030, 1, 5);
+		
+		repository.save(a);
+		repository.save(b);
+		repository.save(c);
+	};
 	}
 
 }
