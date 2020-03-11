bed67590099776a1a8d9092f397052e9c66fb141
lawkr <lawkristian@hotmail.com>
2020-03-11 14:07:52 +0200

    Update

--- bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.bookstore.model.domain.Book;
 import com.example.bookstore.model.domain.BookRepository;
+import com.example.bookstore.model.domain.Category;
+import com.example.bookstore.model.domain.CategoryRepository;
 
 
 
@@ -19,16 +21,25 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository) {
 	return (args) -> {
-		Book a = new Book("Harry Potter", "J.K Rowlings", 101010, 2009, 20);
-		Book b = new Book("Aku Ankka", "Don Rosa", 202020, 2005, 5);
-		Book c = new Book("Raamattu", "Multiple authors", 303030, 1, 5);
 		
-		repository.save(a);
-		repository.save(b);
-		repository.save(c);
-	};
+		crepository.save(new Category("Fiction"));
+		crepository.save(new Category("Fact"));
+		crepository.save(new Category("Fantasy"));
+		
+		brepository.save(new Book("Harry Potter", "J.K Rowlings", 101010, 2009, 20, crepository.findByName("Fantasy").get(0)));
+		brepository.save(new Book("Aku Ankka", "Don Rosa",  202020, 2005, 5, crepository.findByName("Fiction").get(0)));
+		brepository.save(new Book("Raamattu", "Multiple authors", 303030, 1, 5, crepository.findByName("Fact").get(0)));
+		
+	
+		
+		Object log;
+		log.info("fetch all books");
+		for (Book book : brepository.findAll()) {
+			log.info(book.toString());
 	}
 
+	};
+	}
 }
