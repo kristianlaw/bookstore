77bec260cbdcffa8a39fb9e0c5cb897a2e0b4a5e
lawkr <lawkristian@hotmail.com>
2020-03-28 13:47:27 +0200

    Update

--- bookstore/src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.bookstore;
 
+import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.bookstore.BookController.BookController;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BookstoreApplicationTests {
+public class BookstoreApplicationTests {
 
+	@Autowired
+	private BookController controller;
+	
+	
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
